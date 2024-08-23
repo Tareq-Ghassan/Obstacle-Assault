6409c412abeabc1f9f6ac644000d7147e944e6ab
Tareq Ghassan <67103763+Tareq-Ghassan@users.noreply.github.com>
2024-08-23 21:09:06 +0300

    <Moving Platforms C++>

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,13 +15,34 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	StartLocation = GetActorLocation();
 }
 
 // Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	/********* Move Platform ***********/
+	// Get actor location
+	FVector currentLocation = GetActorLocation();
+	
+	// Add vector to that location
+	currentLocation +=  (PlatformVelocity * DeltaTime);
 
+	// Set actor location
+	SetActorLocation(currentLocation);
+
+	/********* Send palatform back if gone too far ***********/
+	float DistanceMoved = FVector::Dist(StartLocation, currentLocation);
+	// Check how far we've moved
+	if (DistanceMoved > MoveDistance) {
+		// Reversed direction of motion if gone too far
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		StartLocation += (MoveDirection * MoveDistance);
+		SetActorLocation(StartLocation);
+		PlatformVelocity = -PlatformVelocity;
+	}
+	
 }
 
